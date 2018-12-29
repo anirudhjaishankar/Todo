d9adc32ffcbc6ba627f2e5e6433ecac1d675144b
anirudhjaishankar <anirudhjaishankar97@gmail.com>
2018-12-29 14:06:15 +0530

    Final commit 1

--- js/master.js
+++ js/master.js
@@ -2,17 +2,19 @@
 
 $(document).ready(function(){
 $('#addtodo').click(function(){
-  $(
-  "<div class='box' id = 'ele'>"
-  +"<div class = 'item' id='item'>"
-      +$('#input')[0].value
-        +'<a id="del-button" class="delete is-danger" ></a>'
-  +'</div>'
-  +'</div>'
-).appendTo($('#todoul')[0]);
+  if($('#input')[0].value != ''){
+    $(
+    "<div class='box'>"
+    +"<div class = 'item' id='item'>"
+        +$('#input')[0].value
+          +'<a id="del-button" class="delete is-danger" ></a>'
+    +'</div>'
+    +'</div>'
+  ).appendTo($('#todoul')[0]);
+}
 // console.log($('#todoul #del-button'));
 });
 $(document).on('click','#del-button',function(){
   $(this)[0].parentNode.parentNode.remove();
-});                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                $('#todoul').sortable();    
+});                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                $('#todoul').sortable();
 });
